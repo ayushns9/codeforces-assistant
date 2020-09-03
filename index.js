2925e3de3b7313156ea376b88d251df4904368e3
Ayush Goel <ayush@pop-os.localdomain>
2020-09-03 13:48:47 +0530

    Addded heroku webhook

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const { setupRegister } = require('./Telegram/setupRegister');
 const { pool } = require('./init/db');
 const {conductMatch} = require('./codeforces/conductMatch');
 
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://codeforces-assistant-telegram.herokuapp.com/';
+
+
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+
 //Setup bot commands
 setupStart(bot);
 setupRegister(bot, pool);
